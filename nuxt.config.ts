6f7c6118e489feda73161335a82a9f932c19795d
quyetpv <quyetpv@bssd.vn>
2026-03-23 17:16:31 +0700

    update google-site-verification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
       title: 'Focus Sanctuary - PomoHaven',
       meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'google-site-verification', content: 'RHhxxun6E5ymGBmc0VjBPyUQ7ZPPOs9uwdXRkUpjbEQ' },
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
